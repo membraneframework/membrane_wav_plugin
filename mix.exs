4ee73e797ddf798fcc7d43e93c5b2b26dc46c7b0
sheldak <samuel.heldak@swmansion.com>
2021-07-29 10:37:00 +0200

    update mix files

--- mix.exs
+++ mix.exs
@@ -1,24 +1,20 @@
-defmodule Membrane.Template.Mixfile do
+defmodule Membrane.WAV.Plugin.Mixfile do
   use Mix.Project
 
   @version "0.1.0"
-  @github_url "https://github.com/membraneframework/membrane_template_plugin"
+  @github_url "https://github.com/membraneframework/membrane_wav_plugin"
 
   def project do
     [
-      app: :membrane_template_plugin,
+      app: :membrane_wav_plugin,
       version: @version,
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-
-      # hex
-      description: "Template Plugin for Membrane Multimedia Framework",
+      description: "Membrane Multimedia Framework plugin for WAV",
       package: package(),
-
-      # docs
-      name: "Membrane Template plugin",
+      name: "Membrane WAV Plugin",
       source_url: @github_url,
       homepage_url: "https://membraneframework.org",
       docs: docs()
@@ -31,15 +27,17 @@ defmodule Membrane.Template.Mixfile do
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_env), do: ["lib"]
 
   defp deps do
     [
       {:membrane_core, "~> 0.7.0"},
+      {:membrane_caps_audio_raw, "~> 0.4.0"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
-      {:credo, "~> 1.5", only: :dev, runtime: false}
+      {:credo, "~> 1.5", only: :dev, runtime: false},
+      {:membrane_file_plugin, "~> 0.6.0", only: :test},
+      {:membrane_ffmpeg_swresample_plugin, "~> 0.7.0", only: :test}
     ]
   end
 
@@ -50,7 +48,8 @@ defmodule Membrane.Template.Mixfile do
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
-      }
+      },
+      files: ["lib", "mix.exs", "README*", "LICENSE*", ".formatter.exs"]
     ]
   end
 
@@ -59,7 +58,7 @@ defmodule Membrane.Template.Mixfile do
       main: "readme",
       extras: ["README.md", "LICENSE"],
       source_ref: "v#{@version}",
-      nest_modules_by_prefix: [Membrane.Template]
+      nest_modules_by_prefix: [Membrane.WAV]
     ]
   end
 end
