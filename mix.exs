74be4475d76e5fac2ebbea1953894c5bc3c21c8f
Jakub Balinski <balinski.jakub@gmail.com>
2022-03-18 11:10:43 +0100

    bump core to 0.9.0

--- mix.exs
+++ mix.exs
@@ -31,13 +31,13 @@ defmodule Membrane.WAV.Plugin.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.8.1"},
-      {:membrane_caps_audio_raw, "~> 0.5.0"},
-      {:membrane_file_plugin, "~> 0.8.0", optional: true},
+      {:membrane_core, "~> 0.9.0"},
+      {:membrane_caps_audio_raw, "~> 0.6.1"},
+      {:membrane_file_plugin, "~> 0.9.0", optional: true},
       {:ex_doc, "~> 0.26", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:credo, "~> 1.6", only: :dev, runtime: false},
-      {:membrane_ffmpeg_swresample_plugin, "~> 0.10.0", only: :test}
+      {:membrane_ffmpeg_swresample_plugin, "~> 0.11.0", only: :test}
     ]
   end
 
