8d4a2a8c9c52e6dfeaa22e8f3e4af53d6240e2bf
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-05 14:23:12 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.WAV.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.10.1"
+  @version "0.10.2"
   @github_url "https://github.com/membraneframework/membrane_wav_plugin"
 
   def project do
